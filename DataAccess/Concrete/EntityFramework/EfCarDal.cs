a96d93f8023022763840c37b5c4edd40c3abd204
Aygul Dikmen <ayguldikmen@gmail.com>
2021-04-26 09:31:05 +0300

    12. Ders Sonu

---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,38 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq.Expressions;
+using Entities.Dtos;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : EfRepositoryBase<Car, RentACarContext>, ICarDal
+    {
+        public List<CarDetailDto> GetCarDetails(Expression<Func<Car, bool>> filter = null)
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                var result = from cars in filter is null ? context.Cars : context.Cars.Where(filter)
+                             join brands in context.Brands
+                             on cars.BrandId equals brands.Id
+                             join colors in context.Colors
+                             on cars.ColorId equals colors.Id
+                             select new CarDetailDto
+                             {
+                                 Id = cars.Id,
+                                 CarName = cars.CarName,
+                                 BrandName = brands.BrandName,
+                                 ColorName = colors.ColorName,
+                                 DailyPrice = cars.DailyPrice,
+                                 ModelYear = cars.ModelYear
+                             };
+
+                return result.ToList();
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -0,0 +1,47 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfRentalDal : EfRepositoryBase<Rental, RentACarContext>, IRentalDal
+    {
+        public List<RentalDetailDto> GetRentalDetails(Expression<Func<Rental, bool>> filter = null)
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                var result = from rentals in filter is null ? context.Rentals : context.Rentals.Where(filter)
+                             join cars in context.Cars
+                             on rentals.CarId equals cars.Id
+                             join customers in context.Customers
+                             on rentals.CustomerId equals customers.Id
+                             join users in context.Users
+                             on customers.UserId equals users.Id
+                             join brands in context.Brands
+                             on cars.BrandId equals brands.Id
+                             join colors in context.Colors
+                             on cars.ColorId equals colors.Id
+                             select new RentalDetailDto
+                             {
+                                 Id = rentals.Id,
+                                 CarName=cars.CarName,
+                                 CustomerName=users.FirstName,
+                                 CompanyName=customers.CompanyName,
+                                 BrandName=brands.BrandName,
+                                 ColorName=colors.ColorName,
+                                 ModelYear=cars.ModelYear,
+                                 RentDate=rentals.RentDate,
+                                 ReturnDate=rentals.ReturnDate
+                             };
+
+                return result.ToList();
+            }
+        }
+    }
+}
